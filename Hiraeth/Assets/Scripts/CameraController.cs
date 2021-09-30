253713fdc36c7e16689074174fe20fa2a8f76e08
Gracisism <gracisism123@gmail.com>
2021-09-29 17:41:35 -0700

    I was baffled (also I worked on the Labryinth thing)
    what

---
+++ Hiraeth/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CameraController : MonoBehaviour
+{
+   public GameObject player;
+   Vector3 distance;
+   public float turnspeed = 0.5f;
+   private Vector3 offset;
+   private Space offsetposition = Space.Self;
+   void Start()
+   {
+       distance = this.transform.position - player.transform.position;
+   }
+ 
+   void Update()
+   {
+       if(offsetposition == Space.Self)
+       {
+           transform.position = player.transform.TransformPoint(distance);
+       }
+       else 
+       {
+           transform.position = player.transform.position + distance;
+       }
+          transform.rotation = player.transform.rotation;
+   }
+}
