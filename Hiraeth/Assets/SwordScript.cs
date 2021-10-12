545b23c222632dc0d2a2924de126d4b25e943b21
Gracisism <gracisism123@gmail.com>
2021-10-11 17:07:32 -0700

    created the 2nd level
    hi

---
+++ Hiraeth/Assets/SwordScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+    public float speed = 2f;
+    public float maxRotation = 45f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(maxRotation * Mathf.Sin(Time.time * speed), 0f, 0f);
+    }
+}
