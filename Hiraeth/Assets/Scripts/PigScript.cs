a20abc4062a1520bc8c8fa6ce77d4823e434cd7c
Gracisism <gracisism123@gmail.com>
2021-11-01 17:07:04 -0700

    worked on the pig, who is trying to kill a rat
    rat come back

--- Hiraeth/Assets/Scripts/PigScript.cs
+++ Hiraeth/Assets/Scripts/PigScript.cs
@@ -7,10 +7,15 @@ public class PigScript : MonoBehaviour
     private Animation anim;
     public float speed = 1.0f;
     public Transform target;
+    private Transform pointA;
+    private Transform pointB;
     // Start is called before the first frame update
     void Start()
     {
         anim = this.gameObject.GetComponent<Animation>();
+        //pointA = this.transform.position;
+        //pointB = target.transform.position;
+        
     }
 
     // Update is called once per frame
@@ -18,18 +23,22 @@ public class PigScript : MonoBehaviour
     {
         float step = speed * Time.deltaTime;
         this.transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-        //Debug.Log("Walking");
         anim.Play("Walk01");
+        
 
         //Check if pig has reached target position 
         if(Vector3.Distance(transform.position, target.position) < 0.001f){
-            //Swap the position of the target
-            target.position *= -1.0f;
-            //Rotate pig to turn around and walk the opposite direction
-            //transform.rotation = new Vector3(transform.position.x, 0f, transform.position.y);
-            transform.Rotate(transform.position.x, 0.0f, transform.position.z, Space.Self);
+            transform.localRotation *= Quaternion.Euler(0, 180, 0);
 
-            //need to have pig turn around in opposite direction; both lines of code above did not work 
+            //print(target.transform.position.x);
+            
+            
+            if(target.transform.position.x == 100){
+                target.transform.position = new Vector3(134, target.transform.position.y, target.transform.position.z);
+            }
+            else{
+                target.transform.position = new Vector3(100, target.transform.position.y, target.transform.position.z);
+            }
         }
     }
 }
