cf4c01a905bfbe680b1f61999b5d8b016da0ef9e
Gracisism <gracisism123@gmail.com>
2021-11-22 17:07:08 -0800

    daksjdajdslkajsdlkajsdlkajsldaksdjalksj work done????????? final level
    the teacher made me write that ok

---
+++ Hiraeth/Assets/Scripts/StumpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StumpScript : MonoBehaviour
+{
+    public float xPos;
+    public int length = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(xPos + Mathf.PingPong(Time.time, length), transform.position.y, transform.position.z);
+    }
+}
